61a8399030f48c6db73fb2792ac3bee62c8552fa
Nilin Abrahamsen <nilin@berkeley.edu>
2022-09-16 19:06:12 -0700

    .

--- pick_and_run_in_display.py
+++ pick_and_run_in_display.py
@@ -1,3 +1,4 @@
+from cancellations.examples  
 from cancellations.display import cdisplay
 from cancellations.utilities import tracking, browse, batchjob, sysutil
 import os
@@ -5,30 +6,32 @@ import re
 import importlib
 
 
-fname='test'
+fname='execprocess'
 
 batch = tracking.Profile(name='run a single script')
 
 batch.name1='browse'
 batch.task1 = browse.Browse
 batch.genprofile1 = lambda _: browse.getdefaultprofile().butwith(
-    msg='select a file to run "{}(*args,**kwargs)" with the args just provided to singlescript.py'.format(fname),
+    msg='select a file to run "{}(process)"'.format(fname),
     parentfolder='cancellations',
     onlyone=True,
     regex='(./)?cancellations.*[a-z].py',
-    condition1=lambda path: re.search('def run', sysutil.readtextfile(path)),
+    condition1=lambda path: re.search('def {}'.format(fname), sysutil.readtextfile(path)),
     readinfo=lambda path: sysutil.readtextfile(path)
 )
 
 
+def execprocess(process):
+    mname = process.path.replace('/', '.')[:-3]
+    m = importlib.import_module(mname)
+    run=tracking.Run(m.getdefaultprofile(),process.display)
+    m.execprocess(run)
 
+batch.name2='run script'
+batch.task2=tracking.newprocess(execprocess)
+batch.genprofile2=lambda prevoutputs: tracking.Profile(path=prevoutputs[1])
 
 if __name__=='__main__':
     [path] = cdisplay.session_in_display(batchjob.Batchjob, batch)
-    mname = path.replace('/', '.')[:-3]
-    print('todo: use importlib to run')
-    print(mname)
 
-    #importlib.import_module(mname)
-    m = importlib.import_module(mname)
-    getattr(m, fname)(*sysutil.cmdparams, **sysutil.cmdredefs)
