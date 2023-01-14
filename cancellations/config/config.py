d64d9e8b74d2fb0ec6c37467ba3c4b5fdd9e8518
Nilin Abrahamsen <nilin@berkeley.edu>
2023-01-14 03:38:13 -0800

    delete stuff

--- cancellations/config/config.py
+++ cancellations/config/config.py
@@ -7,27 +7,5 @@ if not '32' in sys.argv:
 display_on=True
 debug=False
 
-postcommands=[]
-postprocesses=[]
-defaultinputs=dict()
-
-def run_afterdisplayclosed():
-    globals()['display_on']=False
-
-    for postrun in postprocesses:
-        postrun.run_as_NODISPLAY()
-
-    for postcommand in postcommands:
-        postcommand()
-
 biasinitsize=.1
 initweight_coefficient=2
-layernormalization=None
-plotfineness=50
-
-def agrees(d1,**d2):
-    return all([d1[k]==d2[k] for k in d1.keys() if k in d2.keys()])
-
-def test():
-    print(agrees(dict(a=1,b=2,c=3),b=1))
-    print(agrees(dict(a=1,b=2,c=3),b=2))
